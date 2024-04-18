3579eb0ef96a82d80031a8cd194ea23a32313230
Tymon Marek <117597583+TymonMarek@users.noreply.github.com>
2024-04-18 20:39:12 +0100

    Add IProcessArgs interface and update Bot and Logger classes

--- src/base/interfaces/ILogger.ts
+++ src/base/interfaces/ILogger.ts
@@ -13,26 +13,27 @@ export default interface ILogger {
 	 * @description Logs an info message to the console and log file.
 	 * @param message
 	 */
-	Info(message: string, saveToFile?: boolean): Promise<void>;
+	Info(message: string): Promise<void>;
 
 	/**
 	 * @name Warn
 	 * @description Logs a warning message to the console and log file.
 	 * @param message
 	 */
-	Warn(message: string, saveToFile?: boolean): Promise<void>;
+	Warn(message: string): Promise<void>;
 
 	/**
 	 * @name Error
-	 * @description Logs an error message to the console and log file.
+	 * @description Logs an error message to the console and log file, then throws that error.
 	 * @param err
+	 * @never
 	 */
-	Error(err: Error, saveToFile?: boolean): Promise<void>;
+	Error(err: Error): Promise<never>;
 
 	/**
 	 * @name Debug
 	 * @description Logs a debug message to the console and log file.
 	 * @param message
 	 */
-	Debug(message: string, saveToFile?: boolean): Promise<void>;
+	Debug(message: string): Promise<void>;
 }
