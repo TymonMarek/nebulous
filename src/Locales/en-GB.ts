b8f8cd2fd654a7015a297e70d440a287c7d966ff
Tymon Marek <tymon.marek@ulur.org>
2025-02-10 18:18:28 +0000

    chore: Fix incorrect locale name for en-GB.

--- src/Locales/en-GB.ts
+++ src/Locales/en-GB.ts
@@ -5,12 +5,12 @@ import { Locale } from "discord.js";
 export default class EnglishGB extends TranslationTable {
     constructor() {
         super({
-            languageName: "English (Great Britain)",
+            languageName: "English (United Kingdom)",
             localeCode: Locale.EnglishGB,
             enabled: true,
             
             translations: {
-                [TranslationKey.UnknownCommandReply]: "I'm not sure how you got here... This command does not exist!",
+                [TranslationKey.UnknownCommandReply]: "This command does not exist. Please wait a few seconds for Discord to refresh slash commands or restart your Discord client.",
                 [TranslationKey.PingReply]: (ping: number) => `Pong! (${ping}ms)`,
             },
         });
